87f005f1ca7ba29dba4d60884937ca9655a0bceb
mingjerli <mingjerli@gmail.com>
2016-01-12 20:22:50 -0500

    first working version

--- R/igraphinshiny.R
+++ R/igraphinshiny.R
@@ -1,6 +1,25 @@
-
-igraphinshiny <- function(){
-  options(warn=FALSE,show.error.messages = FALSE)
-  shiny::runApp("igraphPlotDemo")
-}
-
+#
+# igraphinshiny <- function(){
+#   options(warn=FALSE,show.error.messages = FALSE)
+#   shiny::runApp("igraphPlotDemo")
+#   #shiny::runApp("R")
+# }
+#
+# igraphinshiny2 <- function(){
+#   options(warn=FALSE,show.error.messages = FALSE)
+#   shiny::runApp("inst/igraphPlotDemo")
+#   #shiny::runApp("R")
+# }
+#
+# igraphinshiny3 <- function(){
+#   options(warn=FALSE,show.error.messages = FALSE)
+#   shiny::runApp("//ecnbwn06p/data/mlee2/R/shiny_testing/igraphPlotDemo")
+#   #shiny::runApp("R")
+# }
+#
+# igraphinshiny4 <- function(){
+#   options(warn=FALSE,show.error.messages = FALSE)
+#   shiny::runApp(paste0(.libPaths()[1],"/igraphPlotDemo"))
+#   #shiny::runApp("R")
+# }
+#
